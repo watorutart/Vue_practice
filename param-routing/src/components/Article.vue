917139fa09d6926c9c800cd8f25909cbce5c383b
watorutart <watoru374215@gmail.com>
2021-04-29 15:26:17 +0900

    ルートパラメータの取得

---
+++ param-routing/src/components/Article.vue
@@ -0,0 +1,10 @@
+<script>
+export default {
+  methods: {
+    onclick: function () {
+      this.$router.push('top')
+    }
+  },
+  template: '<span>id: {{ $route.params.id }}</span>'
+}
+</script>
---
+++ param-routing/src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Article from '@/components/Article'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/articles/:id',
+      name: 'Article',
+      component: Article
+    },
+    {
+      path: '/top',
+      redirect: '/'
+    }
+  ]
+})
